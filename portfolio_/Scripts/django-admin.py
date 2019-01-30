a76f0714bb2a9470c8543117c3fe4d2b6990b193
bag131 <bag131@txstate.edu>
2019-01-30 13:26:01 -0600

    reorganized git repo

--- portfolio_/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\jungl\onedrive\desktop\portfolio\portfolio_\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
