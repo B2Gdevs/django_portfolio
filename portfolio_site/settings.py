60f24cbd707ce3cb5fe69318966888c0afdaecca
ben <benjamingarrard5279@gmail.com>
2019-03-25 18:58:36 -0500

    moved again

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -14,14 +14,16 @@ import os
 import django_heroku
 import dj_database_url
 
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
 try:
     import local_setup
 except ModuleNotFoundError as e:
     print(e)
     print("If viewing this error onn Heroku then everything is fine.")
 
-# Activate Django-Heroku.
-django_heroku.settings(locals())
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
