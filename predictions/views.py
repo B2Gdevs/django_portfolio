a43f80b8e725ad49442547fd085669b93a22fa88
ben <benjamingarrard5279@gmail.com>
2019-03-25 16:54:11 -0500

    completed the nba prediction integration.

--- predictions/views.py
+++ predictions/views.py
@@ -1,9 +1,12 @@
 """
 Views for rendering html and performing actions on data passed to the app.
 
+python 3.7 is used.  This is a dependency that must be known for pickle.
+
 Author: Benjamin Garrard
 """
 from django.shortcuts import render
+from predictions import utils
 
 
 def nba_view(request):
@@ -21,5 +24,27 @@ def nba_view(request):
         The rendered page with any information passed to it through django.
 
     """
-    context = {}
+    result = None
+    display_result = False
+    team = None
+    op_team = None
+    quarter = None
+    if request.method == "POST":
+        team = request.POST.get("team")
+        op_team = request.POST.get("opposing_team")
+        quarter = request.POST.get("quarter")
+        result = utils.predict(team, op_team, quarter)
+        display_result = True
+
+    team_names = utils.load_binary("predictions/pickled/data/unique_teams.pkl")
+    quarters = utils.load_binary("predictions/pickled/data/quarters.pkl")
+    context = {
+        "team_names": team_names,
+        "quarters": quarters,
+        "result": result,
+        "display_result": display_result,
+        "team": team,
+        "op_team": op_team,
+        "quarter": quarter
+    }
     return render(request, 'nba_prediction.html', context)
--- static/js/custom.js
+++ static/js/custom.js
@@ -1,20 +1,62 @@
+
+if (localStorage.getItem("dynamic-text-color") === "dark"){
+darkTheme();
+}
+
 document.getElementById("btn-dark-theme").addEventListener('click', darkTheme);
 document.getElementById("btn-light-theme").addEventListener('click', lightTheme);
+document.getElementById("normal-disclaimer").addEventListener('click', normalDisclaimer);
+document.getElementById("data-disclaimer").addEventListener('click', dataDislcaimer);
 
-function darkTheme(event){
+function darkTheme(){
     document.body.style.backgroundColor = "#1E1E1E";
     let shownText = document.getElementsByClassName("dynamic-text");
+
+    let darkBtn = document.getElementById("btn-dark-theme");
+    let lightBtn = document.getElementById("btn-light-theme");
+
+    darkBtn.style.color = "#FFFFFF";
+    lightBtn.style.color = "#FFFFFF";
 
     for (let text of shownText){
         text.style.color = "#FFFFFF";
     }
+    localStorage.setItem("dynamic-text-color", "dark");
 }
 
-function lightTheme(event){
-    document.body.style.backgroundColor = "#FFFFFF";
+function lightTheme(){
+    document.body.style.backgroundColor = "#F5F5F5";
     let shownText = document.getElementsByClassName("dynamic-text");
+
+    let darkBtn = document.getElementById("btn-dark-theme");
+    let lightBtn = document.getElementById("btn-light-theme");
+
+    lightBtn.style.color = "#1E1E1E";
 
     for (let text of shownText){
         text.style.color = "#1E1E1E";
     }
+
+    localStorage.setItem("dynamic-text-color", "light");
+}
+
+function normalDisclaimer(){
+    let disclaimer = document.getElementById("disclaimer");
+
+    disclaimer.innerText = 
+    `This predicter isn't very good at determining every time a team will be under twenty points in a quarter. ` 
+    + `So it misses a few times where the outcome actually would be that the team would score under 20.  So it`
+    + `is a little difficult to trust the team won't be under twenty.  However, the inverse isn't true.  This `
+    + `predictor is very reliable in its true values.  So if it says a team will be under twenty points in the `
+    + `quarter then it is more than likely right.  So in better words you can't trust the false values, but you `
+    + `can trust the true values.`;
+}
+
+function dataDislcaimer(){
+    let disclaimer = document.getElementById("disclaimer");
+
+    disclaimer.innerText =
+     `The model has 100% precision and 15% recall on 63380 samples. The predictor is an SVM using the `
+     + `RBF kernel.  There were no optimizations done, just the default parameters in Scikit-learn`
+                        
 }
