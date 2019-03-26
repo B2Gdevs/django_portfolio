2ce29e7496478bb2c469bacb16f27ddb5f83d233
ben <benjamingarrard5279@gmail.com>
2019-03-25 20:20:34 -0500

    the false value evaluates to true with bool()

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -33,7 +33,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ["SECRET_KEY"] 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(os.environ.get("DEBUG", "False")
+DEBUG = bool(os.environ.get("DEBUG", False)
 
 # DEBUG_PROPAGATE_EXCEPTIONS = True
 
