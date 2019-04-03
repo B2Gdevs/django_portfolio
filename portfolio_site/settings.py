67181a2f568ed844568755a8169ac23666574dcf
ben <benjamingarrard5279@gmail.com>
2019-04-03 13:08:24 -0500

    fixed linear search title and linearSearch.js typo in the linearsearch.html

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -139,7 +139,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     ]
 
-if False:
+# this needs to be changed to false when running collectstatic, change back
+# when done.
+if DEBUG:
 
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
